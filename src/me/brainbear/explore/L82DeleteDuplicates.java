cc336acf71b965519d8377efd4276283211fb6d7
BrainBear <liancanxiong@outlook.com>
2018-10-30 23:26:39 +0800

    L82 删除排序列表中的重复元素

---
+++ src/me/brainbear/explore/L82DeleteDuplicates.java
@@ -0,0 +1,65 @@
+package me.brainbear.explore;
+
+public class L82DeleteDuplicates {
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if(null == head){
+            return null;
+        }
+
+        ListNode cur = head;
+        ListNode next = cur.next;
+
+        while (next != null) {
+            while (next != null && next.val == cur.val) {
+                next = next.next;
+            }
+
+            cur.next = next;
+            cur = next;
+        }
+
+        return head;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    private static ListNode createTestList(int[] nums) {
+        ListNode head = new ListNode(0);
+        ListNode cur = head;
+        for (int i = 0; i < nums.length; i++) {
+            ListNode listNode = new ListNode(nums[i]);
+            cur.next = listNode;
+            cur = listNode;
+        }
+        return head.next;
+    }
+
+    private static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + "->");
+            head = head.next;
+        }
+        System.out.println("null");
+    }
+
+
+    public static void main(String[] args) {
+        L82DeleteDuplicates deleteDuplicates = new L82DeleteDuplicates();
+
+        ListNode testList = createTestList(new int[]{1, 1, 2});
+        printList(testList);
+        printList(deleteDuplicates.deleteDuplicates(testList));
+
+        testList = createTestList(new int[]{1, 1, 2, 2, 3, 3});
+        printList(testList);
+        printList(deleteDuplicates.deleteDuplicates(testList));
+    }
+}
